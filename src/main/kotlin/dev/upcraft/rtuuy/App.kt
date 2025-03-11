68bd55bd5c72a0ce90236bb1945026ad822435f6
Up <upcraftedlp@gmail.com>
2025-03-11 10:43:21 +0100

    fix about commands

--- src/main/kotlin/dev/upcraft/rtuuy/App.kt
+++ src/main/kotlin/dev/upcraft/rtuuy/App.kt
@@ -2,14 +2,15 @@ package dev.upcraft.rtuuy
 
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.Kord
+import dev.kord.rest.builder.message.embed
 import dev.kordex.core.ExtensibleBot
 import dev.kordex.core.utils.env
 import dev.kordex.core.utils.envOfOrNull
 import dev.kordex.modules.data.mongodb.mongoDB
-import dev.upcraft.rtuuy.extensions.about.AboutExtension
 import dev.upcraft.rtuuy.extensions.anti_reply_ping.AntiReplyPingExtension
 import dev.upcraft.rtuuy.extensions.ban_sync.BanSyncExtension
 import dev.upcraft.rtuuy.extensions.system_notifications.SystemNotificationExtension
+import dev.upcraft.rtuuy.i18n.Translations
 
 // Get the test server ID from the env vars or a .env file
 val TEST_SERVER_ID = envOfOrNull<Snowflake>("TEST_SERVER")
@@ -34,13 +35,48 @@ suspend fun main() {
 		}
 
 		extensions {
-			add(::AboutExtension)
 			add(::AntiReplyPingExtension)
 			add(::BanSyncExtension)
 			add(::SystemNotificationExtension)
 		}
 
 		mongoDB()
+
+		about {
+			ephemeral = false
+
+			section(
+				Translations.About.ApplicationInfo.commandName,
+				Translations.About.ApplicationInfo.commandDescription
+			) {
+				message {
+					val kord = getKoin().get<Kord>()
+					val info = getAppInfo(kord)
+
+					embed {
+						title = Translations.About.ApplicationInfo.title
+							.translateNamed(info)
+						description = Translations.About.ApplicationInfo.text
+							.translateNamed(info)
+
+						field {
+							name = Translations.About.ApplicationInfo.IdField.title.translateNamed(info)
+							value = Translations.About.ApplicationInfo.IdField.text.translateNamed(info)
+							inline = true
+						}
+						field {
+							name = Translations.About.ApplicationInfo.GitField.title.translateNamed(info)
+							value = Translations.About.ApplicationInfo.GitField.text.translateNamed(info)
+							inline = true
+						}
+
+						footer {
+							text = Translations.About.ApplicationInfo.footer.translateNamed(info)
+						}
+					}
+				}
+			}
+		}
 	}
 
 	bot.start()
